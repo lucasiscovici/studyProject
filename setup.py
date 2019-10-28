a013afa38ab97a3f0a819454ec2da971ef7677f1
luluperet <luluisco@yahoo.fr>
2019-10-28 18:02:08 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'studyProject', 'version.py')) as f:
+    exec(f.read())
+    
+setup(
+    name='studyProject',
+    version=__version__,
+    description='Conveniant Project Framework in Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    project_urls={
+        'Source': 'https://github.com/luluperet/studyProject',
+    },
+    author='Lucas Iscovici',
+    author_email='iscovici.lucas@yahoo.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='framework python project machine learning data science',
+    packages=find_packages(exclude=[]),
+    install_requires=[
+        'scikit-learn==0.20.3',
+        'numpy==1.16.2',
+        'python-interface==0.3.0'
+    ]
+)
