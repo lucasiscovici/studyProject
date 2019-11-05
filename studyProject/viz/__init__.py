ff0819006406caf4913d2bf894b32bb36c2ab7e9
lucas iscovici <lucasiscovici@MacBookProLULU-2.local>
2019-11-05 19:16:43 +0100

    studyProjectGit-Mar  5 nov 2019 19:16:43 CET

--- studyProject/viz/__init__.py
+++ studyProject/viz/__init__.py
@@ -1,7 +1,7 @@
 # from ..base import factoryCls
 from .viz import Viz, plotly_google_colab, enable_plotly_in_cell, disable_plotly_in_cell
 from .pandasNewMethodsPlots import *
-
+from .plotlyNewMethodsPlots import *
 from .studyviz_datas import Study_Datas_Viz
 from .studyviz_datasupervise import Study_DatasSupervise_Viz
 from .studyviz_cvresultats import Study_CvResultats_Viz
--- studyProject/viz/plotlyNewMethodsPlots.py
+++ studyProject/viz/plotlyNewMethodsPlots.py
@@ -1,5 +1,7 @@
 import plotly
+from ..utils import F, merge
 from ..utils import setattrAndReturnSelf
+
 plotly.graph_objs._figure.Figure.update_config=lambda self,**kwargs: setattrAndReturnSelf(self,"_config",merge(self._config,kwargs,F))
 def showshow(self,*args,**kwargs):
   meme=merge(dict(config=self._config),kwargs,F)
