81ec5d2ce0cd1adb9e87c59673b04e2ea781bdd7
lucas iscovici <lucasiscovici@MacBookProLULU-2.local>
2019-11-05 20:34:48 +0100

    studyProjectGit-Mar  5 nov 2019 20:34:48 CET

--- studyProject/viz/plotlyNewMethodsPlots.py
+++ studyProject/viz/plotlyNewMethodsPlots.py
@@ -5,5 +5,5 @@ from ..utils import setattrAndReturnSelf
 plotly.graph_objs._figure.Figure.update_config=lambda self,**kwargs: setattrAndReturnSelf(self,"_config",merge(self._config,kwargs,F))
 def showshow(self,*args,**kwargs):
   meme=merge(dict(config=self._config),kwargs,F)
-  return self.show(self,*args,**meme) if hasattr(self,"_config") else self.show(*args,**kwargs)
+  return self.show(*args,**meme) if hasattr(self,"_config") else self.show(*args,**kwargs)
 plotly.graph_objs._figure.Figure.showWithConfig=showshow
