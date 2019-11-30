252578cda77d739cc31b5edcbecf45cf1223777d
Your Name <you@example.com>
2019-11-30 17:10:39 +0100

    studyProjectGit-Sam 30 nov 2019 17:10:38 CET

--- studyProject/utils/speedMLNewMethods.py
+++ studyProject/utils/speedMLNewMethods.py
@@ -60,6 +60,7 @@ def saveLast_(self,func,*args,**kwargs):
 def saveLast2_(self,func,*args,**kwargs):
   realSelf=kwargs.pop("realSelf",self)
   type_=kwargs.pop("type_")
+  print("saveLoad2",self,func,args,kwargs,realSelf,type_)
   if "train" in type_:
     realSelf._lastlastTrain=copy(realSelf._lastTrain)
     realSelf._lastTrain=copy(realSelf.train)
